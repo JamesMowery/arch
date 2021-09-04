12de5d15510eefd88f00a23e0fe75624d75d1d40
James Mowery <jmowery@gmail.com>
2021-09-04 11:39:28 -0700

    Updates arch install script with missing software

--- arch-install-core-software.sh
+++ arch-install-core-software.sh
@@ -15,6 +15,9 @@
 # sudo pacman -Rs --noconfirm <name>
 ####################################################
 
+# TODO: Add multilib to pacman config
+# multilib config here
+
 # Terminal
 
 sudo pacman -S --noconfirm --needed kitty
@@ -30,6 +33,8 @@ sudo pacman -S --noconfirm --needed git
 sudo pacman -S --noconfirm --needed man-db
 sudo pacman -S --noconfirm --needed unzip
 sudo pacman -S --noconfirm --needed mlocate
+sudo pacman -S --noconfirm --needed ntfs-3g
+sudo pacman -S --noconfirm --needed xautolock
 
 # File Browser
 
@@ -43,10 +48,14 @@ sudo pacman -S --noconfirm --needed hwinfo
 sudo pacman -S --noconfirm --needed bpytop
 sudo pacman -S --noconfirm --needed conky
 
+# Notifications
+
+sudo pacman -S --noconfirm --needed dunst
+
 # Browser
 
 sudo pacman -S --noconfirm --needed firefox
-sudo pacman -S --noconfirm --needed chromium
+#sudo pacman -S --noconfirm --needed chromium
 
 # Chat
 
@@ -55,6 +64,7 @@ sudo pacman -S --noconfirm --needed discord
 # Multimedia
 
 sudo pacman -S --noconfirm --needed mpv
+sudo pacman -S --noconfirm --needed feh
 #sudo pacman -S --noconfirm --needed vlc
 sudo pacman -S --noconfirm --needed youtube-dl
 #sudo pacman -S --noconfirm --needed deadbeef
@@ -73,15 +83,21 @@ sudo pacman -S --noconfirm --needed lutris
 
 sudo pacman -S --noconfirm --needed nitrogen
 sudo pacman -S --noconfirm --needed neofetch
-#sudo pacman -S --noconfirm --needed rofi
+sudo pacman -S --noconfirm --needed rofi
 
 # Fonts
+
 sudo pacman -S --noconfirm --needed ttf-fira-code
 sudo pacman -S --noconfirm --needed ttf-fira-mono
 sudo pacman -S --noconfirm --needed ttf-fira-sans
+sudo pacman -S --noconfirm --needed adobe-source-code-pro-fonts
+sudo pacman -S --noconfirm --needed nerd-fonts-source-code-pro
 
 # Audio
 
 sudo pacman -S --noconfirm --needed pulseaudio
 #sudo pacman -S --noconfirm --needed pulse-asla
 sudo pacman -S --noconfirm --needed pavucontrol
+
+# Develop and Compile
+sudo pacman -S --noconfirm --needed base-devel
