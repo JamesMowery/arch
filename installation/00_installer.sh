2f612f584500dfad70a48e4f17c18109e51e37ae
James Mowery <jmowery@gmail.com>
2022-02-21 19:04:45 -0700

    Fixes curl commands for installer

--- installation/00_installer.sh
+++ installation/00_installer.sh
@@ -3,6 +3,6 @@ curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/02
 curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/03_mount.sh
 curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/04_pacstrap.sh
 curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/05_base_system.sh
-curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/06_software_base.sh
-curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/07_software_extra.sh
-#curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/
+curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/06_software_core.sh
+curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/07_software_base.sh
+curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/08_software_extra.sh
