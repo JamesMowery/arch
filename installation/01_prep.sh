a9302dc03e60b5134dc7c0632db1676f32f52efa
James Mowery <jmowery@gmail.com>
2022-02-21 20:52:15 -0700

    Adds clarity for prep and partitioning

--- installation/01_prep.sh
+++ installation/01_prep.sh
@@ -1,3 +1,4 @@
 timedatectl set-ntp true
 reflector --country 'United States' --latest 25 --age 24 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+pacman -Syyy
 ./02_partitioning.sh
--- installation/02_partitioning.sh
+++ installation/02_partitioning.sh
@@ -1,5 +1,11 @@
 lsblk -f
-echo "Run: fdisk /dev/<device> & create partitions as needed."
-echo "Run: mkfs.ext4 /dev/<root_partition> for / partition"
-echo "Run: mkfs.vfat /dev/<boot_partition> for boot partition (if needed)"
-echo "Run: mkswap /dev/<swap_partition> then swapon /dev/<swap_partition> (if needed)"
+echo "Run: fdisk /dev/sdX & press "g" for GPT drive."
+echo "Press 'n' and create a 300MB partition."
+echo "Press 't' and select '1' for 'EFI System'."
+echo "Create other partitions as necessary."
+echo "Press 'w' to write, then format the partitions."
+
+echo "Run: mkfs.vfat /dev/<boot_partition> for EFI System partition."
+echo "Run: mkfs.ext4 /dev/<root_partition> for / partition."
+echo "Run: mkswap /dev/<swap_partition> then swapon /dev/<swap_partition>."
+
