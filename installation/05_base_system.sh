a827759a79764502d7885a4c0c0a1ed8ecd6d551
James Mowery <jmowery@gmail.com>
2022-02-21 19:57:13 -0700

    Fixes locale.gen targeting wrong location

--- installation/05_base_system.sh
+++ installation/05_base_system.sh
@@ -8,7 +8,7 @@ ln -sf /usr/share/zoneinfo/US/Arizona /etc/localtime
 hwclock --systohc
 
 # Locale
-sed -i '177s/.//' /etc/locale.gen
+sed -i '178s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
