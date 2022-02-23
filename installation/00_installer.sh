1555c29c9e47010495f5579028608039cdafd8f7
James Mowery <jmowery@gmail.com>
2022-02-22 20:58:24 -0700

    Removes auto run of 01 script

--- installation/00_installer.sh
+++ installation/00_installer.sh
@@ -5,4 +5,3 @@ curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/04
 curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/05_base_system.sh
 curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/06_software_core.sh
 curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/07_software_extra.sh
-./01_prep.sh
