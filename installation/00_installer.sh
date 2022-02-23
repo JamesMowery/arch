8093ff5f41686295fc0545547f611d637d22114a
James Mowery <jmowery@gmail.com>
2022-02-22 20:29:03 -0700

    Fixes installation script

--- installation/00_installer.sh
+++ installation/00_installer.sh
@@ -1,9 +1,8 @@
 curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/01_prep.sh
-curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/02_partitioning.sh
+curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/02_partition.sh
 curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/03_mount.sh
 curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/04_pacstrap.sh
 curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/05_base_system.sh
 curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/06_software_core.sh
-curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/07_software_base.sh
-curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/08_software_extra.sh
+curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/07_software_extra.sh
 ./01_prep.sh
