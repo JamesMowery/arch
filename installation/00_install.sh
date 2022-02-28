c2f48faa8821cca51a895ea887adc1c604656724
James Mowery <jmowery@gmail.com>
2022-02-27 19:08:11 -0700

    Update 00_install.sh

--- installation/00_install.sh
+++ installation/00_install.sh
@@ -2,6 +2,8 @@ curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/01
 curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/02_partition.sh
 curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/03_mount.sh
 curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/04_pacstrap.sh
-curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/05_base_system.sh
+curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/05_base.sh
 curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/06_software_core.sh
 curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/07_software_extra.sh
+curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/08_kde.sh
+curl -LO https://raw.githubusercontent.com/JamesMowery/arch/main/installation/08_kde_minimal.sh
