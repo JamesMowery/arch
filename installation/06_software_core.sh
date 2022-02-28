1c7dcee5166fcb66245a43b4d55c04f03ba14fed
James Mowery <jmowery@gmail.com>
2022-02-27 19:21:17 -0700

    Update 06_software_core.sh

--- installation/06_software_core.sh
+++ installation/06_software_core.sh
@@ -17,19 +17,18 @@
 # BASE SOFTWARE
 ################################################################################
 
-sudo pacman -S --noconfirm --needed base-devel bluez bluez-utils cups cups-pdf gvfs mesa networkmanager nvidia nvidia-utils nvidia-settings openssh os-prober pipewire pipeware-alsa pipewire-alsa pipewire-pulse pipewire-jack plocate reflector util-linux unzip xdg-user-dirs xdg-utils
-
-
-# Boot Manager (Grub)
-################################################################################
-sudo pacman -S --noconfirm --needed grub efibootmgr
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+sudo pacman -S base-devel bluez bluez-utils cups cups-pdf gvfs hplip networkmanager nvidia nvidia-utils openssh os-prober pipewire pipewire-alsa pipewire-pulse pipewire-jack plocate reflector util-linux unrar unzip xdg-user-dirs xdg-utils
 
 # Boot Manager (Refind)
 ################################################################################
 #sudo pacman -S --noconfirm --needed refind
 #refind-install
+
+# Boot Manager (Grub)
+################################################################################
+#sudo pacman -S --noconfirm --needed grub efibootmgr
+#grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+#grub-mkconfig -o /boot/grub/grub.cfg
 
 ################################################################################
 # EXTRA SOFTWARE
@@ -53,9 +52,13 @@ pacman -S --noconfirm --needed firefox
 
 systemctl enable NetworkManager
 systemctl enable bluetooth
-systemctl enable cups.socket
+systemctl enable cups
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
+systemctl enable nvidia-suspend
+
+# Update Default Shell To Fish
+chsh -s /usr/bin/fish
 
 echo "---------------------------------------"
 echo "Continue with 07_software_extra.sh"
