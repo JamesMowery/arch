4b39d5b069d881096c481f737cdf07d4deee032e
James Mowery <jmowery@gmail.com>
2022-02-28 23:49:26 -0700

    Adds missing printer dependency

--- installation/08_kde_minimal.sh
+++ installation/08_kde_minimal.sh
@@ -2,7 +2,7 @@
 sudo pacman -S plasma-desktop sddm sddm-kcm
 
 # KDE Core
-sudo pacman -S bluedevil breeze breeze-gtk discover kde-cli-tools kde-gtk-config kdecoration kdeplasma-addons khotkeys kinfocenter kmenuedit kscreen kscreenlocker kwin libkscreen libksysguard oxygen plasma-disks plasma-firewall plasma-integration plasma-nm plasma-pa plasma-systemmonitor plasma-workspace plasma-workspace-wallpapers polkit-kde-agent systemsettings xdg-desktop-portal-kde
+sudo pacman -S bluedevil breeze breeze-gtk discover kde-cli-tools kde-gtk-config kdecoration kdeplasma-addons khotkeys kinfocenter kmenuedit kscreen kscreenlocker kwin libkscreen libksysguard oxygen plasma-disks plasma-firewall plasma-integration plasma-nm plasma-pa plasma-systemmonitor plasma-workspace plasma-workspace-wallpapers polkit-kde-agent systemsettings system-config-printer xdg-desktop-portal-kde
 
 # KDE Software
 sudo pacman -S ark dolphin dolphin-plugins filelight kate kcalc kcharselect kcolorchooser kdenetwork-filesharing kdf kdialog kompare ksystemlog okular partitionmanager print-manager spectacle 
